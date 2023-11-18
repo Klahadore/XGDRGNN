4efddab69973a69e2f6f21692d79cf80299827e0
Klahadore <98653242+Klahadore@users.noreply.github.com>
2023-11-18 15:40:25 -0500

    defined network

--- basicDataProcessing.py
+++ basicDataProcessing.py
@@ -8,8 +8,10 @@ import pandas as pd
 
 from torch_geometric.data import Data, download_url, extract_gz
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 url = 'http://snap.stanford.edu/biodata/datasets/10012/files/DG-AssocMiner_miner-disease-gene.tsv.gz'
-extract_gz(download_url(url,'data'), 'data')
+extract_gz(download_url(url, 'data'), 'data')
 
 data_path = "data/DG-AssocMiner_miner-disease-gene.tsv"
 
@@ -20,6 +22,7 @@ def load_node_mapping(datafile_path, index_col, offset=0):
     mapping = {index_id: i + offset for i, index_id in enumerate(df.index.unique())}
     return mapping
 
+
 # Generates edge list in terms of node indices.
 def load_edge_list(datafile_path, src_col, src_mapping, dst_col, dst_mapping):
     df = pd.read_csv(datafile_path, sep="\t")
@@ -28,6 +31,7 @@ def load_edge_list(datafile_path, src_col, src_mapping, dst_col, dst_mapping):
     edge_index = torch.tensor([src_nodes, dst_nodes])
 
     return edge_index
+
 
 def initialize_data(datafile_path, num_features=1):
     dz_col, gene_col = "# Disease ID", "Gene ID"
@@ -54,5 +58,18 @@ print(data_object)
 print("Number of genes:", len(gene_mapping))
 print("Number of diseases:", len(dz_mapping))
 
+# Data Transformations
+# Splits data into train, test, and eval
+transform = T.Compose([
+    T.NormalizeFeatures(),
+    T.ToDevice(device),
+    T.RandomLinkSplit(num_val=0.05, num_test=0.15, is_undirected=True, split_labels=True,
+                      add_negative_train_samples=True)
+])
 
+train_dataset, val_dataset, test_dataset = transform(data_object)
+
+print("Train Data:\n", train_dataset)
+print("Validation Data:\n", val_dataset)
+print("Test Data:\n", test_dataset)
 
---
+++ basicModel.py
@@ -0,0 +1,30 @@
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch_geometric.transforms as T
+import torch
+from torch_geometric.nn import GAE, GCNConv, VGAE
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+NUM_FEATURES = 20
+HIDDEN_SIZE = 200
+OUT_CHANNELS = 20
+EPOCHS = 40
+
+class GCNEncoder(torch.nn.Module):
+    def __init__(self, in_channels, hidden_size, out_channels, dropout):
+        super(GCNEncoder, self).__init__()
+
+        self.conv1 = GCNConv(in_channels, hidden_size, cached=True)
+        self.conv2 = GCNConv(hidden_size, out_channels, cached=True)
+        self.droupout(dropout)
+
+    def forward(self, x, edge_index):
+        x_temp1 = self.conv2(x,edge_index).relu()
+        x_temp2 = self.droupout(x_temp1)
+        return self.conv2(x_temp2, edge_index)
+
+gae_model = GAE(GCNEncoder(NUM_FEATURES, HIDDEN_SIZE, OUT_CHANNELS, 0.5))
+gae_model = gae_model.to(device)
