a9cf6039bb5237f36f1c6d3281f1b5062006aa93
Klahadore <98653242+Klahadore@users.noreply.github.com>
2023-11-19 10:26:05 -0500

    added data visualization

--- basicModel.py
+++ basicModel.py
@@ -13,7 +13,8 @@ HIDDEN_SIZE = 200
 OUT_CHANNELS = 20
 EPOCHS = 40
 
-class GCNEncoder(torch.nn.Module):
+
+class GCNEncoder(nn.Module):
     def __init__(self, in_channels, hidden_size, out_channels, dropout):
         super(GCNEncoder, self).__init__()
 
@@ -25,6 +26,7 @@ class GCNEncoder(torch.nn.Module):
         x_temp1 = self.conv2(x,edge_index).relu()
         x_temp2 = self.droupout(x_temp1)
         return self.conv2(x_temp2, edge_index)
+
 
 gae_model = GAE(GCNEncoder(NUM_FEATURES, HIDDEN_SIZE, OUT_CHANNELS, 0.5))
 gae_model = gae_model.to(device)
