25a6c05f1d995ee87cee12430b86b942ce7a0bb6
Klahadore <98653242+Klahadore@users.noreply.github.com>
2023-11-26 16:12:45 -0500

    added embedding functions and dataset

--- embeddings.py
+++ embeddings.py
@@ -3,7 +3,7 @@ from torch_geometric.data import Data, download_url, extract_gz
 
 DISEASE_DATASET_URL = "https://snap.stanford.edu/biodata/datasets/10021/files/D-DoMiner_miner-diseaseDOID.tsv.gz"
 
-
+#1
 def gene_embedding(id):
     return torch.ones(20)
 
