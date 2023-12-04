070619b8471a0db28f0510d29a568f17a45d5215
Klahadore <98653242+Klahadore@users.noreply.github.com>
2023-12-04 14:44:48 -0500

    model v1

--- data.py
+++ data.py
@@ -8,6 +8,8 @@ from collections import OrderedDict
 import pickle
 import os
 import sys
+from torch_geometric import transforms as T
+
 
 from embeddings import *
 
@@ -22,20 +24,6 @@ pickled_data_path = "data/pickled_data.pickle"
 """
     Check if pickled data exists and is not empty
 """
-if os.path.exists(pickled_data_path) and os.path.getsize(pickled_data_path) > 0:
-    try:
-        with open(pickled_data_path, 'rb') as file:
-            dataset = pickle.load(file)
-
-        if dataset:
-            print("Loaded pickled data")
-            sys.exit(0)
-        else:
-            print("Pickled data is empty")
-    except Exception as e:
-        print(f"Failed to load pickled data due to an error: {e}")
-else:
-    print("Pickled data file does not exist or is empty")
 
 
 def build_file_mapping(filename):
@@ -67,42 +55,6 @@ def build_index_map_of_values(filename, index_map):
 """
 Builds node to index maps for each type of node
 """
-gene_to_index = {}
-disease_to_index = {}
-chemical_to_index = {}
-phe_to_index = {}
-mutation_to_index = {}
-pathway_to_index = {}
-
-build_index_map_of_keys("gene_disease.json", gene_to_index)
-build_index_map_of_keys("gene_mutation.json", gene_to_index)
-build_index_map_of_keys("gene_phe.json", gene_to_index)
-build_index_map_of_keys("gene_pathway.json", gene_to_index)
-build_index_map_of_keys("gene_chemical.json", gene_to_index)
-build_index_map_of_keys("gene_gene.json", gene_to_index)
-build_index_map_of_values("gene_gene.json", gene_to_index)
-build_index_map_of_values("disease_gene.json", gene_to_index)
-
-build_index_map_of_keys("disease_gene.json", disease_to_index)
-build_index_map_of_keys("disease_mutation.json", disease_to_index)
-build_index_map_of_keys("disease_phe.json", disease_to_index)
-build_index_map_of_keys("disease_pathway.json", disease_to_index)
-build_index_map_of_keys("disease_chemical.json", disease_to_index)
-build_index_map_of_keys("disease_disease.json", disease_to_index)
-build_index_map_of_values("disease_disease.json", disease_to_index)
-build_index_map_of_values("gene_disease.json", disease_to_index)
-
-build_index_map_of_values("disease_chemical.json", chemical_to_index)
-build_index_map_of_values("gene_chemical.json", chemical_to_index)
-
-build_index_map_of_values("disease_phe.json", phe_to_index)
-build_index_map_of_values("gene_phe.json", phe_to_index)
-
-build_index_map_of_values("disease_mutation.json", mutation_to_index)
-build_index_map_of_values("gene_mutation.json", mutation_to_index)
-
-build_index_map_of_values("disease_pathway.json", pathway_to_index)
-build_index_map_of_values("gene_pathway.json", pathway_to_index)
 
 
 def create_node_embedding_tensor(index_map, embedding_function):
@@ -110,8 +62,6 @@ def create_node_embedding_tensor(index_map, embedding_function):
     for key in index_map:
         tensor_list.append(embedding_function(key))
     return torch.stack(tensor_list)
-
-
 
 
 def create_edge_indices(file_mapping, index_map_1, index_map_2):
@@ -131,44 +81,150 @@ def create_edge_indices(file_mapping, index_map_1, index_map_2):
     return edge_indices
 
 
+def build_dataset():
+    gene_to_index = {}
+    disease_to_index = {}
+    chemical_to_index = {}
+    phe_to_index = {}
+    mutation_to_index = {}
+    pathway_to_index = {}
+
+    build_index_map_of_keys("gene_disease.json", gene_to_index)
+    build_index_map_of_keys("gene_mutation.json", gene_to_index)
+    build_index_map_of_keys("gene_phe.json", gene_to_index)
+    build_index_map_of_keys("gene_pathway.json", gene_to_index)
+    build_index_map_of_keys("gene_chemical.json", gene_to_index)
+    build_index_map_of_keys("gene_gene.json", gene_to_index)
+    build_index_map_of_values("gene_gene.json", gene_to_index)
+    build_index_map_of_values("disease_gene.json", gene_to_index)
+
+    build_index_map_of_keys("disease_gene.json", disease_to_index)
+    build_index_map_of_keys("disease_mutation.json", disease_to_index)
+    build_index_map_of_keys("disease_phe.json", disease_to_index)
+    build_index_map_of_keys("disease_pathway.json", disease_to_index)
+    build_index_map_of_keys("disease_chemical.json", disease_to_index)
+    build_index_map_of_keys("disease_disease.json", disease_to_index)
+    build_index_map_of_values("disease_disease.json", disease_to_index)
+    build_index_map_of_values("gene_disease.json", disease_to_index)
+
+    build_index_map_of_values("disease_chemical.json", chemical_to_index)
+    build_index_map_of_values("gene_chemical.json", chemical_to_index)
+
+    build_index_map_of_values("disease_phe.json", phe_to_index)
+    build_index_map_of_values("gene_phe.json", phe_to_index)
+
+    build_index_map_of_values("disease_mutation.json", mutation_to_index)
+    build_index_map_of_values("gene_mutation.json", mutation_to_index)
+
+    build_index_map_of_values("disease_pathway.json", pathway_to_index)
+    build_index_map_of_values("gene_pathway.json", pathway_to_index)
+
+    dataset = HeteroData()
+
+    dataset['gene'].x = create_node_embedding_tensor(gene_to_index, gene_embedding)
+    dataset['disease'].x = create_node_embedding_tensor(disease_to_index, disease_embedding)
+    dataset['chemical'].x = create_node_embedding_tensor(chemical_to_index, chemical_embedding)
+    dataset['phe'].x = create_node_embedding_tensor(phe_to_index, phe_embedding)
+    dataset['mutation'].x = create_node_embedding_tensor(mutation_to_index, mutation_embedding)
+    dataset['pathway'].x = create_node_embedding_tensor(pathway_to_index, pathway_embedding)
+
+    dataset['gene', 'gene_disease', 'disease'].edge_index = create_edge_indices(
+        build_file_mapping("gene_disease.json"),
+        gene_to_index, disease_to_index)
+    dataset['gene', 'gene_chemical', 'chemical'].edge_index = create_edge_indices(
+        build_file_mapping("gene_chemical.json"),
+        gene_to_index, chemical_to_index)
+    dataset['gene', 'gene_phe', 'phe'].edge_index = create_edge_indices(build_file_mapping("gene_phe.json"),
+                                                                               gene_to_index, phe_to_index)
+    dataset['gene', 'gene_mutation', 'mutation'].edge_index = create_edge_indices(
+        build_file_mapping("gene_mutation.json"),
+        gene_to_index, mutation_to_index)
+    dataset['gene', 'gene_pathway', 'pathway'].edge_index = create_edge_indices(
+        build_file_mapping("gene_pathway.json"),
+        gene_to_index, pathway_to_index)
+    dataset['gene', 'gene_gene', 'gene'].edge_index = create_edge_indices(build_file_mapping("gene_gene.json"),
+                                                                                gene_to_index, gene_to_index)
+    dataset['disease', 'disease_chemical', 'chemical'].edge_index = create_edge_indices(
+        build_file_mapping("disease_chemical.json"),
+        disease_to_index, chemical_to_index)
+    dataset['disease', 'disease_phe', 'phe'].edge_index = create_edge_indices(
+        build_file_mapping("disease_phe.json"),
+        disease_to_index, phe_to_index)
+    dataset['disease', 'disease_mutation', 'mutation'].edge_index = create_edge_indices(
+        build_file_mapping("disease_mutation.json"),
+        disease_to_index, mutation_to_index)
+    dataset['disease', 'disease_pathway', 'pathway'].edge_index = create_edge_indices(
+        build_file_mapping("disease_pathway.json"),
+        disease_to_index, pathway_to_index)
+    dataset['disease', 'disease_disease', 'disease'].edge_index = create_edge_indices(
+        build_file_mapping("disease_disease.json"),
+        disease_to_index, disease_to_index)
+
+    return dataset
 
 
-dataset = HeteroData()
+dataset = None
+if os.path.exists(pickled_data_path) and os.path.getsize(pickled_data_path) > 0:
+    try:
+        with open(pickled_data_path, 'rb') as file:
+            dataset = pickle.load(file)
+    except:
+        print("data set failed to load")
 
-dataset['gene'].x = create_node_embedding_tensor(gene_to_index, gene_embedding)
-dataset['disease'].x = create_node_embedding_tensor(disease_to_index, disease_embedding)
-dataset['chemical'].x = create_node_embedding_tensor(chemical_to_index, chemical_embedding)
-dataset['phe'].x = create_node_embedding_tensor(phe_to_index, phe_embedding)
-dataset['mutation'].x = create_node_embedding_tensor(mutation_to_index, mutation_embedding)
-dataset['pathway'].x = create_node_embedding_tensor(pathway_to_index, pathway_embedding)
+    if dataset:
+        print("Loaded pickled data")
 
-dataset['gene', 'associated_with', 'disease'].edge_index = create_edge_indices(build_file_mapping("gene_disease.json"),
-                                                                               gene_to_index, disease_to_index)
-dataset['gene', 'associated_with', 'chemical'].edge_index = create_edge_indices(build_file_mapping("gene_chemical.json"),
-                                                                                gene_to_index, chemical_to_index)
-dataset['gene', 'associated_with', 'phe'].edge_index = create_edge_indices(build_file_mapping("gene_phe.json"),
-                                                                            gene_to_index, phe_to_index)
-dataset['gene', 'associated_with', 'mutation'].edge_index = create_edge_indices(build_file_mapping("gene_mutation.json"),
-                                                                                gene_to_index, mutation_to_index)
-dataset['gene', 'associated_with', 'pathway'].edge_index = create_edge_indices(build_file_mapping("gene_pathway.json"),
-                                                                                gene_to_index, pathway_to_index)
-dataset['gene', 'associated_with', 'gene'].edge_index = create_edge_indices(build_file_mapping("gene_gene.json"),
-                                                                                 gene_to_index, gene_to_index)
-dataset['disease', 'associated_with', 'chemical'].edge_index = create_edge_indices(build_file_mapping("disease_chemical.json"),
-                                                                                    disease_to_index, chemical_to_index)
-dataset['disease', 'associated_with', 'phe'].edge_index = create_edge_indices(build_file_mapping("disease_phe.json"),
-                                                                                disease_to_index, phe_to_index)
-dataset['disease', 'associated_with', 'mutation'].edge_index = create_edge_indices(build_file_mapping("disease_mutation.json"),
-                                                                                    disease_to_index, mutation_to_index)
-dataset['disease', 'associated_with', 'pathway'].edge_index = create_edge_indices(build_file_mapping("disease_pathway.json"),
-                                                                                    disease_to_index, pathway_to_index)
-dataset['disease', 'associated_with', 'disease'].edge_index = create_edge_indices(build_file_mapping("disease_disease.json"),
-                                                                                  disease_to_index, disease_to_index)
+    else:
+        print("Pickled data is empty")
+        dataset = build_dataset()
+        with open(pickled_data_path, 'wb') as file:
+            pickle.dump(dataset, file)
+        print("pickled dataset saved to file")
+else:
+    print("Pickled data file does not exist or is empty")
+    dataset = build_dataset()
+    with open(pickled_data_path, 'wb') as file:
+        pickle.dump(dataset, file)
+    print("Pickled data saved to file")
 
 
-print(dataset)
+dataset = T.ToUndirected()(dataset)
 
-with open(pickled_data_path, 'wb') as file:
-    pickle.dump(dataset, file)
 
-print("Pickled data saved")
+
+
+transform = T.RandomLinkSplit(
+    num_val=0.1,
+    num_test=0.2,
+    add_negative_train_samples=True,
+    edge_types=("gene", "gene_disease", "disease",),
+    rev_edge_types=("disease", "rev_gene_disease", "gene",)
+
+)
+train_dataset, val_dataset, test_dataset = transform(dataset)
+
+
+
+print(train_dataset.node_types)
+print(train_dataset.edge_types)
+print(train_dataset["phe"].x)
+
+print(len(train_dataset["gene", "gene_disease", "disease"].edge_label_index[0]))
+print(len(test_dataset["gene", "gene_disease", "disease"].edge_label_index[0]))
+print(len(val_dataset["gene", "gene_disease", "disease"].edge_label_index[0]))
+
+print(train_dataset)
+#
+# for edge_type in dataset.metadata()[1]:
+#     top, middle , bottom = edge_type
+#     edge_indices = dataset[top, middle, bottom].edge_index
+#     for top_index in edge_indices[0]:
+#         if top_index >= len(dataset[top].x):
+#             print(top)
+#     for bottom_index in edge_indices[1]:
+#         if bottom_index >= len(dataset[bottom].x):
+#             print(bottom)
+#     print(f"just completed {top} and {bottom} for {edge_type}")
+
+print(train_dataset["gene", "gene_disease", "disease"].edge_label_index)
+print(train_dataset["gene", "disease"].edge_label)
--- model.py
+++ model.py
@@ -1,65 +1,74 @@
 import torch
-from torch_geometric.nn import GATConv, Linear, to_hetero, GCNConv, GAE
-from data import train_dataset, num_gene
+from torch_geometric.nn import GATConv, Linear, to_hetero, GCNConv, GAE, SAGEConv
+from torch_geometric.data import HeteroData
 import torch.nn.functional as F
-from torch.optim import optimizer
+
+from torch_geometric.utils import remove_self_loops, contains_self_loops
 from sklearn.metrics import roc_auc_score
 from visualize import visualize_graph, visualize_emb
+import torch_geometric.transforms as T
+
+from data import train_dataset, test_dataset, val_dataset
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
-# visualize_graph(train_dataset)
-# visualize_graph(test_dataset)
-
-
 class GCN(torch.nn.Module):
     def __init__(self, hidden_channels, out_channels):
-        super().__init__()
-        self.conv1 = GCNConv(hidden_channels, 2 * out_channels)
-        self.conv2 = GCNConv(2 * out_channels, out_channels)
+        super(GCN, self).__init__()
+        self.conv1 = SAGEConv((-1, -1), hidden_channels)
+        self.conv2 = SAGEConv((-1, -1), out_channels)
 
-    def forward(self, x, edge_index, visualize=False):
-        x = self.conv1(x, edge_index)
-        x = F.relu(x)
-        x = F.dropout(x, p=0.5, training=self.training)
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index).relu()
         x = self.conv2(x, edge_index)
-        # if visualize:
-        #     visualize_emb(x)
         return x
 
+class EdgeDecoder(torch.nn.Module):
+    def __init__(self, hidden_channels):
+        super().__init__()
+        self.lin1 = Linear(2 * hidden_channels, hidden_channels)
+        self.lin2 = Linear(hidden_channels, 1)
 
-hidden_channels, out_channels = train_dataset.num_features, 16
+    def forward(self, z_dict, edge_label_index):
+        row, col = edge_label_index
+        z = torch.cat([z_dict['gene'][row], z_dict['disease'][col]], dim=-1)
+        z = self.lin1(z).relu()
+        z = self.lin2(z)
+        return z.view(-1)
 
-model = GAE(GCN(hidden_channels, out_channels)).to(device)
+class Model(torch.nn.Module):
+    def __init__(self, hidden_channels):
+        super().__init__()
+        self.encoder = GCN(hidden_channels, hidden_channels)
+        self.encoder = to_hetero(self.encoder, train_dataset.metadata(), aggr="sum")
+        self.decoder = EdgeDecoder(hidden_channels)
 
+    def forward(self, x_dict, edge_index_dict, edge_label_index):
+        z_dict = self.encoder(x_dict, edge_index_dict)
+        return self.decoder(z_dict, edge_label_index)
+
+
+model = Model(hidden_channels=20).to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
 
 def train():
     model.train()
     optimizer.zero_grad()
-    z = model.encode(train_dataset.x, train_dataset.edge_index)
-    loss = model.recon_loss(z, train_dataset.edge_index)
+    pred = model.forward(train_dataset.x_dict, train_dataset.edge_index_dict, train_dataset['gene','gene_disease', 'disease'].edge_label_index)
+    target = train_dataset['gene', 'gene_disease', 'disease'].edge_label
+    loss = torch.nn.BCEWithLogitsLoss()(pred, target)
     loss.backward()
     optimizer.step()
     return float(loss)
 
 
-
-
 for epoch in range(200):
     print(train())
 
-# Assuming training is done
-# model.eval()
-# with torch.no_grad():
-#     # You can pass in any dataset here (train_dataset or test_dataset)
-#     model(train_dataset.x.to(device), train_dataset.edge_index.to(device), visualize=True)
-# print model embeddings
 
 with torch.no_grad():
     print(model.encode(train_dataset.x, train_dataset.edge_index))
 
-visualize_emb(model.encode(train_dataset.x, train_dataset.edge_index).detach(), num_gene)
 
