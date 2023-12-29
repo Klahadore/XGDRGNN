02ddb2ce522c54e1a3bdf145d2c4f47e3fa4f51c
Klahadore <98653242+Klahadore@users.noreply.github.com>
2023-12-29 16:05:50 -0500

    refactored

--- embeddings.py
+++ embeddings.py
@@ -2,16 +2,17 @@ import torch
 import requests
 import json
 import pickle
-
+import unicodedata
 from sentence_transformers import SentenceTransformer
 
 
 def gene_embedding(id):
-    
     return torch.ones(20, dtype=torch.float32)
+
 
 def gene_embedding(id):
     return torch.ones(20, dtype=torch.float32)
+
 
 def convert_string(string):
     newlist = list(string.split(" "))
@@ -19,16 +20,16 @@ def convert_string(string):
 
 
 def symptom_description(path_to_file):
-    with open('symptom_data.pkl', 'rb') as fp:
+    with open('data/symptom_data.pkl', 'rb') as fp:
         symptom_dictionary = pickle.load(fp)
-    #symptom_dictionary = {}
+    # symptom_dictionary = {}
     with open(path_to_file, 'r') as file:
-        lines = file.readlines()[223:]      #was 1:
-        for line in lines:                                 #for line in lines:
+        lines = file.readlines()[223:]  # was 1:
+        for line in lines:  # for line in lines:
             columns = line.strip().split('\t')
-            key = columns[3]                                    #was 6
+            key = columns[3]  # was 6
             if key not in symptom_dictionary:
-            #try:                                                     Everything below this needs to be indented if try/except is to be used again FYI
+                # try:                                                     Everything below this needs to be indented if try/except is to be used again FYI
                 url = f'https://id.nlm.nih.gov/mesh/{key}.json'
                 response = requests.get(url)
                 if response.status_code == 200:
@@ -50,19 +51,19 @@ def symptom_description(path_to_file):
                 #     print(f"Request failed: {e}")
                 #     return None
                 symptom_dictionary[key] = [cat]
-    with open('symptom_data.pkl', 'wb') as fp:
+    with open('data/symptom_data.pkl', 'wb') as fp:
         pickle.dump(symptom_dictionary, fp, protocol=pickle.HIGHEST_PROTOCOL, fix_imports=True)
         print('Dictionary saved successfully')
         print(symptom_dictionary)
 
-#input disease id, return embedding based off definiton from API of NCBI
-def disease_and_symptom_embedding(id, path_to_file):
-    pilot = ""
+
+# input disease id, return embedding based off definiton from API of NCBI
+def disease_and_symptom_embedding(id):
     if id[0] == "D":
         try:
             url = f'https://id.nlm.nih.gov/mesh/{id}.json'
             response = requests.get(url)
-    
+
             if response.status_code == 200:
                 y = json.loads(response.content)
                 search = y["preferredConcept"]
@@ -70,7 +71,7 @@ def disease_and_symptom_embedding(id, path_to_file):
                 try:
                     url = search + ".json"
                     response = requests.get(url)
-    
+
                     if response.status_code == 200:
                         y = json.loads(response.content)
                         cat = y["scopeNote"]["@value"]
@@ -81,7 +82,7 @@ def disease_and_symptom_embedding(id, path_to_file):
                 except requests.RequestException as g:
                     print(f"Request failed: {g}")
                     return None
-    
+
             else:
                 print("Failed")
                 return None
@@ -91,58 +92,63 @@ def disease_and_symptom_embedding(id, path_to_file):
     else:
         try:
             url = f'https://id.nlm.nih.gov/mesh/{id}.json'
-        
+
             response = requests.get(url)
-        
+
             if response.status_code == 200:
                 y = json.loads(response.content)
                 cat = y["scopeNote"]["@value"]
-        
+
             else:
                 print("Failed")
                 return None
         except requests.RequestException as e:
             print(f"Request failed: {e}")
             return None
-    
-    print(convert_string(cat))
+
     model = SentenceTransformer('BAAI/bge-small-en-v1.5')
     embeddings = model.encode(cat, show_progress_bar=False)
-    print(embeddings)
+    return embeddings
 
-    with open('symptom_data.pkl', 'rb') as fp:
-        symptom_dictionary = pickle.load(fp)
-        print('symptom_dictionary: ')
-        print(symptom_dictionary)
+    #
+    # with open('symptom_data.pkl', 'rb') as fp:
+    #     symptom_dictionary = pickle.load(fp)
+    #     print('symptom_dictionary: ')
+    #     print(symptom_dictionary)
+    #
+    # data_dictionary = {}
+    #
+    # with open(path_to_file, 'r') as file:
+    #     lines = file.readlines()[1:]
+    #     for line in lines:
+    #         columns = line.strip().split('\t')
+    #         key = columns[2]
+    #         #value = columns[1]
+    #         value = symptom_dictionary[columns[6]]
+    #         if key in data_dictionary:
+    #             if value not in data_dictionary[key]:
+    #                 data_dictionary[key].append(value)
+    #         else:
+    #             data_dictionary[key] = [value]
+    # return embeddings, data_dictionary[pilot]
+    #
 
-    data_dictionary = {}
 
-    with open(path_to_file, 'r') as file:
-        lines = file.readlines()[1:]
-        for line in lines:
-            columns = line.strip().split('\t')
-            key = columns[2]
-            #value = columns[1]
-            value = symptom_dictionary[columns[6]]
-            if key in data_dictionary:
-                if value not in data_dictionary[key]:
-                    data_dictionary[key].append(value)
-            else:
-                data_dictionary[key] = [value]
-    return embeddings, data_dictionary[pilot]
-
-#print(symptom_description('Symptom-Occurence-Output.txt'))
-print(disease_and_symptom_embedding("D012221", 'symptom_data.txt'))
+# print(symptom_description('Symptom-Occurence-Output.txt'))
+print(disease_and_symptom_embedding("D001835"))
 
 
 def chemical_embedding(id):
     return torch.ones(20, dtype=torch.float32)
 
+
 def phe_embedding(id):
     return torch.ones(20, dtype=torch.float32)
 
+
 def mutation_embedding(id):
     return torch.ones(20, dtype=torch.float32)
+
 
 def pathway_embedding(id):
     return torch.ones(20, dtype=torch.float32)
---
+++ symptoms.py
@@ -0,0 +1,8 @@
+import json
+
+from embeddings import disease_and_symptom_embedding
+
+"""
+    Makes a json map of diseases and their associated symptoms. 
+    Outputs 
+"""
