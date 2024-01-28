d7c297edadbf28e2604f644608fed0365f76899f
Klahadore <stringycheese@protonmail.com>
2024-01-28 15:47:15 -0500

    hello

--- model_evaluation.py
+++ model_evaluation.py
@@ -13,21 +13,22 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 with open("data/new_test_dataset.pickle", "rb") as file:
     test_data = pickle.load(file)
     print(test_data.edge_label[: test_data.edge_label_index.shape[1]].shape, "ter")
-    test_data.edge_index = utils.negative_sampling(test_data.edge_index)
+    # test_data.edge_index = utils.negative_sampling(test_data.edge_index)
     print(test_data.edge_index)
     print(test_data.edge_label[: test_data.edge_label_index.shape[1]].shape)
 
-model = Model(384, training=False)
+model = Model(384, training=False).cuda()
 model.load_state_dict(torch.load("alphaModel_2_epochs.pt"))
 model.eval()
 
 data_loader = LinkNeighborLoader(
     test_data,
     num_neighbors=[25, 21],
-    batch_size=512,
+    batch_size=256,
     shuffle=False,
-    edge_label=test_data.edge_label[: test_data.edge_label_index.shape[1]],
+    # edge_label=test_data.edge_label[: test_data.edge_label_index.shape[1]],
     edge_label_index=test_data.edge_label_index,
+    neg_sampling="binary",
     num_workers=4,
 )
 
@@ -40,15 +41,10 @@ with torch.no_grad():
 
     for batch_data in data_loader:
         batch_data = batch_data.to(device)
-
+        batch_data.cuda()
         pred, _ = model(batch_data)
 
         probs = torch.sigmoid(pred).cpu().numpy()
-
-        all_probs.extend(probs[:, 1])
-        all_targets.extend(batch_data.edge_label[: len(pred)].cpu().numpy())
-
-    all_probs = np.array(all_probs)
-    all_targets = np.array(all_targets)
-
-    fpr, tpr, thresholds = roc_auc_score(all_probs, all_targets)
+        target = batch_data.edge_label[: len(pred)].cpu().numpy()
+        print(target)
+        print(roc_auc_score(target, probs))
